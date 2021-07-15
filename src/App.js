d9e3ba717b6390e48048686e93567cfdc8bdf67d
Lucas Boscariole Silva <lucas.boscariole@hotmail.com>
2021-07-15 00:48:44 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -10,31 +10,33 @@ import Error from './pages/Error';
 import Properties from './pages/Properties';
 import Footer from './pages/Footer';
 import SingleHouse from './pages/SingleHouse';
-
+import ScrollToTop from './components/scrollToTop';
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Navbar />
       <Dropdown />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-        <Route exact path='/properties'>
-          <Properties />
-        </Route>
-        <Route exact path='/contact'>
-          <Contact />
-        </Route>
-        <Route exact path='/properties/:id' children={<SingleHouse />} />
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
+      <ScrollToTop>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/properties'>
+            <Properties />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+          <Route exact path='/properties/:id' children={<SingleHouse />} />
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+      </ScrollToTop>
       <Footer />
     </Router>
   );
